75bedd73e027320b8faa9107c0989a6878d071ad
Santiago Valencia <svalenciagarcia707@gmail.com>
2024-07-25 15:53:04 -0500

    Se sube una práctica de POO en Python sobre el sistema solar y Pygame. Se aplican todos los 4 pilares de la POO: Encapsulamiento, Herencia, Abstracción y Polimorfismo

--- asteroid.py
+++
@@ -1,9 +0,0 @@
-from astronomical_object import AstronomicalObject
-
-class Asteroid(AstronomicalObject):
-    def __init__(self, image_path, mass, distance, orbit_speed):
-        super().__init__(image_path=image_path, mass=mass, distance=distance, orbit_speed=orbit_speed)
-
-    # Sobreescritura de método abstracto. No se hace nada porque el asteroide no tendrá campo magnético
-    def generate_magnetic_field(self, screen):
-        pass
